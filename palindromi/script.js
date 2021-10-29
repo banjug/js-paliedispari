318018a3a35fb7423ec2c0429e7d676b1eba2c11
Samuele <miotto.samuele@gmail.com>
2021-10-29 17:14:43 +0200

    add reverse word script and palindrome check

--- palindromi/script.js
+++ palindromi/script.js
@@ -5,10 +5,23 @@
 // -----------------------------------------------------
 
 // inserire una parola e salvarla (variabile prompt)
+let userWord = prompt('Inserisci una parola.');
 
 // capire se la parola è palindroma
-    // invertire la parola per confrontarla (??)
 
-    // se le parole coincidono la parola è palindroma
+// invertire la parola per confrontarla (??)
+let reversed = '';
+for ( let i = userWord.length -1; i >= 0; i--) {
+    reversed += userWord[i];
+}
 
+console.log(reversed);
+
+// se le parole coincidono la parola è palindroma
 // stampare se la parola è palindoma o meno 
+
+if (userWord == reversed) {
+    console.log('La parola è palindroma. ');
+} else {
+    console.log('La parola NON è palindroma');
+}
