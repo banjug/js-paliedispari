7973c260f2a9c53eb8507fac66a9384d3d92154e
Samuele <miotto.samuele@gmail.com>
2021-10-29 17:23:10 +0200

    add function to check for palindrome words

--- palindromi/script.js
+++ palindromi/script.js
@@ -5,23 +5,47 @@
 // -----------------------------------------------------
 
 // inserire una parola e salvarla (variabile prompt)
-let userWord = prompt('Inserisci una parola.');
+// let parola = userWord('Inserisci una parola.');
 
 // capire se la parola è palindroma
 
 // invertire la parola per confrontarla (??)
-let reversed = '';
-for ( let i = userWord.length -1; i >= 0; i--) {
-    reversed += userWord[i];
+// let reversed = '';
+// for ( let i = userWord.length -1; i >= 0; i--) {
+//     reversed += userWord[i];
+// }
+// console.log(reversed);
+
+// // se le parole coincidono la parola è palindroma
+// // stampare se la parola è palindoma o meno 
+
+// if (userWord == reversed) {
+//     console.log('La parola è palindroma. ');
+// } else {
+//     console.log('La parola NON è palindroma');
+// }
+
+// -----------------------------------------------------
+
+// inserire una parola e salvarla (variabile prompt)
+let parola = prompt('Inserisci una parola.');
+
+// dichiarare la funzione per controllare il palindromo
+function reverseWord(userWord) {
+    // invertire la parola per confrontarla
+    let reversed = '';
+    for ( let i = userWord.length -1; i >= 0; i--) {
+        reversed += userWord[i];
+    }
+    console.log(reversed);
+    // // se le parole coincidono la parola è palindroma
+    // // stampare se la parola è palindoma o meno 
+    if (userWord == reversed) {
+        console.log('La parola è palindroma. ');
+    } else {
+        console.log('La parola NON è palindroma');
+    }
 }
 
-console.log(reversed);
-
-// se le parole coincidono la parola è palindroma
-// stampare se la parola è palindoma o meno 
-
-if (userWord == reversed) {
-    console.log('La parola è palindroma. ');
-} else {
-    console.log('La parola NON è palindroma');
-}
+// richiamare la funzione
+reverseWord(parola);
