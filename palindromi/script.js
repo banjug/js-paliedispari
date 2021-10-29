45ae4529c33273fd1b10f1aea7a1215572e1b574
Samuele <miotto.samuele@gmail.com>
2021-10-29 17:02:21 +0200

    add logic steps

---
+++ palindromi/script.js
@@ -0,0 +1,14 @@
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma.
+// Dare l’output relativo.
+
+// -----------------------------------------------------
+
+// inserire una parola e salvarla (variabile prompt)
+
+// capire se la parola è palindroma
+    // invertire la parola per confrontarla (??)
+
+    // se le parole coincidono la parola è palindroma
+
+// stampare se la parola è palindoma o meno 
---
+++ pari-e-dispari/script.js
@@ -0,0 +1,19 @@
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+// -----------------------------------------------------
+
+// l'utente sceglie pari o dispari (boolean)
+
+// l'utente sceglie un numero tra 1 e 5 compresi
+
+// il computer genera il numero che va a competere con l'utente
+
+// si sommano i due numeri 
+
+// stabiliamo se la somma dei due numeri è pari o dispari (x % 2 == 0)
+
+// stampiamo chi ha vinto a seconda della scelta iniziale dell'utente
