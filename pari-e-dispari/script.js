8f2dc916928f2428cafcf719f72960849f4c8b71
Samuele <miotto.samuele@gmail.com>
2021-10-30 11:13:31 +0200

    odd or even working script

--- pari-e-dispari/script.js
+++ pari-e-dispari/script.js
@@ -7,13 +7,48 @@
 // -----------------------------------------------------
 
 // l'utente sceglie pari o dispari (boolean)
+let userChoice = prompt("Pari o dispari?");
+// trasforma la stringa in minuscolo per poi paragonarla anche in caso l'utente la scriva in maiuscolo
+userChoice = userChoice.toLowerCase();
+
+console.log("Il giocatore ha scelto " + userChoice);
+
+let userEven = false;
+if (userChoice == "pari") {
+    userEven = true
+}
+
+console.log(userEven);
 
 // l'utente sceglie un numero tra 1 e 5 compresi
+let userNum = parseInt(prompt("Scegli un numero tra 1 e 5"));
+console.log("L'utente ha il numero " + userNum);
 
 // il computer genera il numero che va a competere con l'utente
+let cpuNum = Math.floor(Math.random() * 5) + 1;
+console.log("Il computer ha il numero " + cpuNum);
 
 // si sommano i due numeri 
+const numSum = userNum + cpuNum;
+console.log("La somma dei due numeri è " + numSum);
 
 // stabiliamo se la somma dei due numeri è pari o dispari (x % 2 == 0)
+let sumEven = false;
+if (numSum % 2 == 0) {
+    sumEven = true;
+}
+console.log(sumEven);
 
 // stampiamo chi ha vinto a seconda della scelta iniziale dell'utente
+let playerWin = false;
+if (userEven == true && sumEven == true) {
+    playerWin = true;
+} else if (userEven == false && sumEven == false) {
+    playerWin = true;
+}
+
+if (playerWin == true) {
+    console.log('Ha vinto il giocatore.');
+} else {
+    console.log('Ha vinto il computer.');
+}
